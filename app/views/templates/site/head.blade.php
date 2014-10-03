bb5056d346a3e7e3a7d123941549194e8ac4b138
Stanislav Chernyavskiy <stress_tn@mail.ru>
2014-10-03 19:36:46 +0400

    jquery local

--- app/views/templates/site/head.blade.php
+++ app/views/templates/site/head.blade.php
@@ -17,8 +17,8 @@
 	{{ HTML::scriptmod('js/vendor/modernizr-2.6.2.min.js') }}
 	{{ HTML::stylemod('css/style.css') }}
 	<script type="text/javascript" src="//yandex.st/share/share.js" charset="utf-8"></script>
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-	<script>window.jQuery || document.write('<script src="js/vendor/jquery.min.js"><\/script>')</script>
+	
+	<script src="js/vendor/jquery.min.js"></script>
 	<script src="js/vendor/jquery.mustache.js" type="text/javascript"></script>
 
 	<!-- svg icon loader -->
