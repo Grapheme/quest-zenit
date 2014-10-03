37a9f1ea6e254e69ef04a209f7706bdfccfafcda
sngrl <reserved@mail.ru>
2014-10-03 19:54:04 +0400

    +

--- app/views/templates/site/scripts.blade.php
+++ app/views/templates/site/scripts.blade.php
@@ -2,6 +2,8 @@
 
     <? include(Helper::inclayout("literal")); ?>
 
+    <!-- {{ Helper::inclayout("literal")); }} -->
+
     {{ HTML::scriptmod("js/vendor/jquery.min.js") }}
     {{ HTML::scriptmod("js/plugins.js") }}
     {{ HTML::scriptmod("js/main.js") }}
