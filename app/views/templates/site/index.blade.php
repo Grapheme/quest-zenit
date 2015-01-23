5371e825a98deec613e0b3cee639f9300107d3c1
sngrl <reserved@mail.ru>
2015-01-23 16:18:59 +0300

    fix

--- app/views/templates/site/index.blade.php
+++ app/views/templates/site/index.blade.php
@@ -41,8 +41,8 @@ $target_amount = (int)$quest->target_amount;
 				<p class="quest__description main-font main-font_gray main-font_light">
 				    {{ $quest->short }}
 				</p>
-				@if (isset($quest->videos) && count($quest->videos))
-					@foreach ($quest->videos as $video)
+				@if (isset($quest->video) && is_array($quest->video) && count($quest->video))
+					@foreach ($quest->video as $video)
 						<p class="quest__description main-font main-font_gray main-font_light">
 							{{ youtubeReplace($video) }}
 						</p>
