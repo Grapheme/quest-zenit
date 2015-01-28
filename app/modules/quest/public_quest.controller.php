1465b1e357e66266fa68d09281faa22b8c1e700f
sngrl <reserved@mail.ru>
2015-01-28 14:52:14 +0300

    fix

--- app/modules/quest/public_quest.controller.php
+++ app/modules/quest/public_quest.controller.php
@@ -96,7 +96,7 @@ class PublicQuestController extends BaseController {
                     $payer_name = @$transaction->payment_full_decoded['userid'];
 
                 if (!$payer_name && $transaction->payment_full != '') {
-                    preg_match('~"userid":"[^"]*?"~is', $transaction->payment_full, $matches);
+                    preg_match('~\"userid\"\:\".*?\"~is', $transaction->payment_full, $matches);
                     $payer_name = @$matches[1];
                 }
 
