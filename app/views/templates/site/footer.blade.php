e593b05cf7927ef31ddbacbb41387f19b66b9cda
Andrey Samoylov <andrey@grapheme.ru>
2015-08-19 11:07:43 +0300

    rusfond

--- app/views/templates/site/footer.blade.php
+++ app/views/templates/site/footer.blade.php
@@ -3,7 +3,7 @@
 		<div class="wrp">
 			<h2 class="block-title main-font main-font_gray main-font_light">Контакты</h2>
 			<p class="middle-font middle-font_white middle-font_center contacts__title">
-				Благотворительный фонд «АдВита»
+				Фонд помощи больным детям «Русфонд»
 			</p>
 			<table class="contacts__container">
 				<tr class="contacts__row  contacts__row_visible">
@@ -13,13 +13,15 @@
 						</div>
 						<div class="contacts-info-block">
 							<p class="middle-font middle-font_white middle-font_opacity">
-								197022, Санкт-Петербург, Каменноостровский проспект, 40, офис 508 (пятый этаж), ст. метро Петроградская
+								125080, город Москва, улица Врубеля, дом 4, строение 1
 							</p>
+							<a href="mailto:rusfond@rusfond.ru" class="middle-font middle-font_white">Почта</a> 
+							<a href="http://rusfond.ru" class="middle-font middle-font_white little-margin">Сайт</a> 
 						</div>
 					</td>
 					<td class="contacts__cell contacts__cell_hide">
 						<p class="middle-font middle-font_white">
-							Телефон диспетчера службы крови: +7 (812) 337-27-33
+							Бесплатные звонки по России: 8-800-250-75-25
 							<br><span class="middle-font__span">прием звонков в будние дни  9.00 — 17.00</span>
 						</p>
 						<p class="middle-font middle-font_white">
@@ -33,32 +35,29 @@
 						</div>
 						<div class="contacts-info-block">
 							<p class="middle-font middle-font_white middle-font_opacity">
-								+7 (812) 337-27-33  факс<br>+7 (901) 300-00-25
+								В Санкт-Петербурге:<br>8-921-424-27-12<br>Руководитель — Брусилова Анна
 							</p>
-							<a href="mailto:mail@advita.ru" class="middle-font middle-font_white">mail@advita.ru</a> 
-							<a href="http://advita.ru" class="middle-font middle-font_white little-margin">advita.ru</a> 
+							<a href="mailto:annarusfond@mail.ru" class="middle-font middle-font_white">Почта</a> 
+							<a href="http://rusfond.ru/spb" class="middle-font middle-font_white little-margin">Сайт</a> 
 						</div>
 					</td>
 				</tr>
 				<tr class="contacts__row contacts__row_hidden">
 					<td class="contacts__cell">
 						<p class="middle-font middle-font_white">
-							Телефон диспетчера службы крови:<br>+7 (812) 337-27-33
-							<br><span class="middle-font__span">прием звонков в будние дни  9.00 — 17.00</span>
+							Бесплатные звонки по России:<br>8-800-250-75-25
 						</p>
 					</td>
 					<td class="contacts__cell contacts__cell_hide"></td>
 					<td class="contacts__cell">
 						<p class="middle-font middle-font_white">
-							Телефон диспетчера службы крови:<br>+7 (901) 300-00-25 
-							<br><span class="middle-font__span">прием звонков в будние дни  9.00 — 17.00</span>
 						</p>
 					</td>
 				</tr>
 			</table>
 			<div class="contacts__documents clearfix">
-				<a href="docs/offer_advita.pdf" target="_blank" class="middle-font middle-font_white middle-font__span left-a">Оферта</a>
-				<a href="docs/pp.pdf" target="_blank" class="middle-font middle-font_white middle-font__span left-a">Оплата и безопасность</a>
+				<a href="http://www.rusfond.ru/about/153" target="_blank" class="middle-font middle-font_white middle-font__span left-a">Оферта</a>
+				<a href="http://www.rusfond.ru/donation" target="_blank" class="middle-font middle-font_white middle-font__span left-a">Как помочь</a>
 				<div class="copyright"><a href="http://funcfunc.ru" target="_blank">Разработка сайта</a> Func</div>
 			</div>
 			<span class="icon icon-arrow_up js-scrollTop">
